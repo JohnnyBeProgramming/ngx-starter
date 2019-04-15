5798ba7e72dc248b4a9b303c3512ff6f750ef3d7
johnathanlodder <johnathan.loodder@realimpactanalytics.com>
2019-04-15 16:58:50 +0200

    add state

--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from '../environments/environment';
 
 export const routes = [
   { path: '', loadChildren: '@test/home' },
@@ -15,6 +18,10 @@ export const routes = [
     BrowserModule,
     NxModule.forRoot(),
     RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- apps/home/src/module.ts
+++ apps/home/src/module.ts
@@ -4,15 +4,16 @@ import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeFeatureComponent } from './app/home-feature/home-feature.component';
+import { HomeState } from './state';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
   declarations: [HomeFeatureComponent],
   imports: [
     CommonModule,
     NxModule.forRoot(),
-    RouterModule.forChild([
-      { path: '', component: HomeFeatureComponent },
-    ])
+    RouterModule.forChild([{ path: '', component: HomeFeatureComponent }]),
+    NgxsModule.forFeature([HomeState])
   ],
   providers: [],
   bootstrap: [HomeFeatureComponent]
